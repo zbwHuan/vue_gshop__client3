2bb823bfffa5c0a451b0d0b1553f5456dbc08b0a
zbwHuan <you@example.com>
2019-06-11 09:45:30 +0800

    scroll

--- src/mock/mock-server.js
+++ src/mock/mock-server.js
@@ -1,8 +1,8 @@
 import Mock from 'mockjs'
 import data from './data.json'
 
-Mock.mock('/goods', {code: 0, data: data.goods})
+Mock.mock('/goods', { code: 0, data: data.goods })
 
-Mock.mock('/ratings', {code: 0, data: data.ratings})
+Mock.mock('/ratings', { code: 0, data: data.ratings })
 
-Mock.mock('/info', {code: 0, data: data.info})
+Mock.mock('/info', { code: 0, data: data.info })
