55c4dcccb7672ab4226b2796cc1a9b1d7ebe313a
zbwHuan <you@example.com>
2019-06-12 08:37:21 +0800

    info

--- src/main.js
+++ src/main.js
@@ -1,22 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import { Button } from 'mint-ui'
 import App from './App.vue'
 import router from './router'
 import Header from './components/Header/Header.vue'
 import Star from './components/Star/Star.vue'
 import CartControl from './components/CartControl/CartControl.vue'
+import Split from './components/Split/Split.vue'
 import store from './vuex/store'
 import './validate'
 import './mock/mock-server'
-import { Button } from 'mint-ui'
+
 
 Vue.config.productionTip = false
 
+Vue.component(Button.name, Button)
 Vue.component('Header', Header)
 Vue.component('Star', Star)
 Vue.component('CartControl', CartControl)
-Vue.component(Button.name, Button)
+Vue.component('Split', Split)
 
 /* eslint-disable no-new */
 new Vue({
